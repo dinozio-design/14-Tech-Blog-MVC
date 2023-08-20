f150f28e7b8398dad2d3f29aac8b162161a12f2a
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-08-20 12:44:43 -0400

    wrote the homeroutes, next is to test it with some seed data

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,26 @@
 const router = require('express').Router();
-const {User, Post, Comment} = require('../models');
-// need to add withAuth here after I write the auth.js in utils
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get('/', async (req,res)=>{
-    try{
-// get all posts and JOIN with user data
-
-// serialize data so the template can read it
-
-// pass serialized datata and session flad into template
-    } catch(err) {
+router.get('/', async (req, res) => {
+    try {
+        // get all posts and JOIN with user data
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['userName'],
+                },
+            ],
+        });
+        // serialize data so the template can read it
+        const posts = postData.map((post) => post.get({ plain: true }));
+        // pass serialized datata and session flad into template
+        res.render('homepage', {
+            posts,
+            // logged_in: req.session.logged_in // commenting this out for now
+        });
+    } catch (err) {
         res.status(500).json(err);
     }
 });
