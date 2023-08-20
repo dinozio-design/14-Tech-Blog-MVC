370b9d6581e2cf3f67826967876929b8d37358e5
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-08-20 11:45:30 -0400

    starting the controllers and the routes first I wrote the index

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const apiRoutes = require(''); //get ./api routes here
+const homeRoutes = require(''); //get ./homeRoutes here
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
